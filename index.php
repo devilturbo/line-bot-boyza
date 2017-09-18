9dda545b2583d25968bd920b9e2117905332b5eb
Toyota_Buzz\suvachat <buzz.devapp.se2@gmail.com>
2017-09-18 15:40:18 +0700

    asd
    asd

--- index.php
+++ index.php
@@ -32,8 +32,7 @@ if($arrJson['events'][0]['message']['text'] == "สวัสดี"){
   $arrPostData['messages'][0]['type'] = "text";
   $arrPostData['messages'][0]['text'] = "ฉันไม่เข้าใจคำสั่ง";
 }
-echo"<pre>";print_r($arrJson); 
- exit;
+
 $ch = curl_init();
 curl_setopt($ch, CURLOPT_URL,$strUrl);
 curl_setopt($ch, CURLOPT_HEADER, false);
