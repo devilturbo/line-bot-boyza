45d3db4556248337a8fbaf27062121060e355476
Toyota_Buzz\suvachat <buzz.devapp.se2@gmail.com>
2017-09-20 17:04:37 +0700

    asd
    asd

--- index.php
+++ index.php
@@ -1,47 +1,47 @@
 <?php
- 
-$strAccessToken = "rRHE7SZwIcoSP/G29igg/qkctcxtYnJBoPlBYVwwzCQG1PL1S9qts/zTFJ21nddCVhq80kLTYXYRD2EDgRdRrR+LLdIaY99CVl6C0YiBFKkWfKt4YcW1myVifQ4RdG3WnK9Zq6bh4RiroaSzIchxNAdB04t89/1O/w1cDnyilFU=";
- 
-$content = file_get_contents('php://input');
-$arrJson = json_decode($content, true);
- 
-$strUrl = "https://api.line.me/v2/bot/profile";
- 
-$arrHeader = array();
-$arrHeader[] = "Content-Type: application/json";
-$arrHeader[] = "Authorization: Bearer {$strAccessToken}";
- 
-if($arrJson['events'][0]['message']['text'] == "สวัสดี"){
-  $arrPostData = array();
-  $arrPostData['replyToken'] = $arrJson['events'][0]['replyToken'];
-  $arrPostData['messages'][0]['type'] = "text";
-  $arrPostData['messages'][0]['text'] = $arrJson['events'][0]['displayName'];
-}else if($arrJson['events'][0]['message']['text'] == "ชื่ออะไร"){
-  $arrPostData = array();
-  $arrPostData['replyToken'] = $arrJson['events'][0]['replyToken'];
-  $arrPostData['messages'][0]['type'] = "text";
-  $arrPostData['messages'][0]['text'] = "ฉันยังไม่มีชื่อนะ";
-}else if($arrJson['events'][0]['message']['text'] == "ทำอะไรได้บ้าง"){
-  $arrPostData = array();
-  $arrPostData['replyToken'] = $arrJson['events'][0]['replyToken'];
-  $arrPostData['messages'][0]['type'] = "text";
-  $arrPostData['messages'][0]['text'] = "ฉันทำอะไรไม่ได้เลยdasdas คุณต้องสอนฉันอีกเยอะ";
-}else{
-  $arrPostData = array();
-  $arrPostData['replyToken'] = $arrJson['events'][0]['replyToken'];
-  $arrPostData['messages'][0]['type'] = "text";
-  $arrPostData['messages'][0]['text'] = "ฉันไม่เข้าใจคำสั่ง";
-}
+$access_token = 'rRHE7SZwIcoSP/G29igg/qkctcxtYnJBoPlBYVwwzCQG1PL1S9qts/zTFJ21nddCVhq80kLTYXYRD2EDgRdRrR+LLdIaY99CVl6C0YiBFKkWfKt4YcW1myVifQ4RdG3WnK9Zq6bh4RiroaSzIchxNAdB04t89/1O/w1cDnyilFU=';
 
-$ch = curl_init();
-curl_setopt($ch, CURLOPT_URL,$strUrl);
-curl_setopt($ch, CURLOPT_HEADER, false);
-curl_setopt($ch, CURLOPT_POST, true);
-curl_setopt($ch, CURLOPT_HTTPHEADER, $arrHeader);
-curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($arrPostData));
-curl_setopt($ch, CURLOPT_RETURNTRANSFER,true);
-curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
-$result = curl_exec($ch);
-curl_close ($ch);
- 
-?>
+// Get POST body content
+$content = file_get_contents('php://input');
+// Parse JSON
+$events = json_decode($content, true);
+// Validate parsed JSON data
+if (!is_null($events['events'])) {
+  // Loop through each event
+  foreach ($events['events'] as $event) {
+    // Reply only when message sent is in 'text' format
+    if ($event['type'] == 'message' && $event['message']['type'] == 'text') {
+      // Get text sent
+      $text = $event['message']['text'];
+      // Get replyToken
+      $replyToken = $event['replyToken'];
+
+      // Build message to reply back
+      $messages = [
+        'type' => 'text',
+        'text' => $text
+      ];
+
+      // Make a POST Request to Messaging API to reply to sender
+      $url = 'https://api.line.me/v2/bot/message/reply';
+      $data = [
+        'replyToken' => $replyToken,
+        'messages' => [$messages],
+      ];
+      $post = json_encode($data);
+      $headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
+
+      $ch = curl_init($url);
+      curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+      curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+      curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
+      curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+      curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+      $result = curl_exec($ch);
+      curl_close($ch);
+
+      echo $result . "\r\n";
+    }
+  }
+}
+echo "OK";
