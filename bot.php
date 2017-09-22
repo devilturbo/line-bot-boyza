3972c4e265248323ed232d2139470e2b90f1e0cb
Toyota_Buzz\suvachat <buzz.devapp.se2@gmail.com>
2017-09-22 14:40:51 +0700

    ฟหก
    ฟหก

--- bot.php
+++ bot.php
@@ -1,23 +1,49 @@
+
+
 <?php
+$access_token = 'rRHE7SZwIcoSP/G29igg/qkctcxtYnJBoPlBYVwwzCQG1PL1S9qts/zTFJ21nddCVhq80kLTYXYRD2EDgRdRrR+LLdIaY99CVl6C0YiBFKkWfKt4YcW1myVifQ4RdG3WnK9Zq6bh4RiroaSzIchxNAdB04t89/1O/w1cDnyilFU=';
 
-include ('line-bot.php');
+// Get POST body content
+$content = file_get_contents('php://input');
+// Parse JSON
+$events = json_decode($content, true);
+// Validate parsed JSON data
+if (!is_null($events['events'])) {
+    // Loop through each event
+    foreach ($events['events'] as $event) {
+        // Reply only when message sent is in 'text' format
+        if ($event['type'] == 'message' && $event['message']['type'] == 'text') {
+            // Get text sent
+            $text = $event['message']['text'];
+            // Get replyToken
+            $replyToken = $event['replyToken'];
 
-$channelSecret = '841486a93a017315b13d5bce4b657adc';
-$access_token  = 'rRHE7SZwIcoSP/G29igg/qkctcxtYnJBoPlBYVwwzCQG1PL1S9qts/zTFJ21nddCVhq80kLTYXYRD2EDgRdRrR+LLdIaY99CVl6C0YiBFKkWfKt4YcW1myVifQ4RdG3WnK9Zq6bh4RiroaSzIchxNAdB04t89/1O/w1cDnyilFU=';
+            // Build message to reply back
+            $messages = [
+                'type' => 'text',
+                'text' => $text
+            ];
 
-$bot = new BOT_API($channelSecret, $access_token);
-	
-if (!empty($bot->isEvents)) {
-		
-    $bot->replyMessageNew($bot->replyToken, json_encode($bot->message));
+            // Make a POST Request to Messaging API to reply to sender
+            $url = 'https://api.line.me/v2/bot/message/reply';
+            $data = [
+                'replyToken' => $replyToken,
+                'messages' => [$messages],
+            ];
+            $post = json_encode($data);
+            $headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
 
-    if ($bot->isSuccess()) {
-        echo 'Succeeded!';
-        exit();
+            $ch = curl_init($url);
+            curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+            curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+            curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
+            curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+            curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+            $result = curl_exec($ch);
+            curl_close($ch);
+
+            echo $result . "\r\n";
+        }
     }
-
-    // Failed
-    echo $bot->response->getHTTPStatus . ' ' . $bot->response->getRawBody(); 
-    exit();
-
 }
+echo "OK";
